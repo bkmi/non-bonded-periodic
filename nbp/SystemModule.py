109a73bc12f058988d38da2edeaa2f78f4e881ec
Ben <benmiller1315@gmail.com>
2017-12-18 21:23:48 +0100

    Made SystemModule more pythonic

--- nbp/SystemModule.py
+++ nbp/SystemModule.py
@@ -1,18 +1,12 @@
 class System:
     """Wrapper for static SystemInfo and state dependent SystemState info."""
-
     def __init__(self, info, states):
-
         self.__systemInfo = info
         self.__systemStates = states
-        pass
 
-    def update_state(self, newState):
+    def update_state(self, new_state):
         """Appends the new state to the systemStates list"""
-
-        self.__systemStates.append(newState)
-
-    pass
+        self.__systemStates.append(new_state)
 
     def info(self):
         """Gives the static information about the system"""
@@ -21,97 +15,69 @@ class System:
     def state(self):
         """GIves the current dynamic information about the system"""
 
-        pass
-
     def states(self):
         """Gives all the dynamic information about the system"""
-
         return self.__systemStates
-
-    pass
-
 
 
 class SystemInfo:
-
-    """
-    This class represents all the static information of the system
+    """This class represents all the static information of the system
 
     __box_dim: an array 1, 2 or 3 dimensional, each cell is a dimension of the box containing the system [W, L, H]
     __particle_info:
     __sigma:
     __cutoff_radius: the radius chosen to do the cutoff
     """
-
     def __init__(self, box, particles, sigma):
-
         self.__box_dim = box
         self.__particle_info = particles
         self.__sigma = sigma
-
-        # sigma * 2.5 is a standard approximation
-        self.__cutoff_radius = sigma * 2.5
-
-        pass
+        self.__cutoff_radius = sigma * 2.5  # sigma * 2.5 is a standard approximation
 
     def box_dim(self):
         """Gives the box dimensions"""
-
         return self.__box_dim
 
     def particle_info(self):
         """Gives the static information about the particles"""
-
         return self.__particle_info
 
     def cutoff(self):
         """Returns the value chosen for the cutoff radius"""
-
         return self.__cutoff_radius
 
 
-
-
 class SystemState:
-
-    """
-    Contains all the dynamic information about the system
+    """Contains all the dynamic information about the system
 
     __positions: the position of the particles
     __electrostatics: the forces, the energies and the potentials of the particles
     __neighbours: the current status of the neighbours
     """
-
     def __init__(self, positions, electrostatics):
-
         self.__positions = positions
         self.__electrostatics = electrostatics
+        self.__neighbours = None  # init the neighbours - don't know yet how
 
-        # init the neighbours - don't know yet how
-
-        pass
-
+    @property
     def positions(self):
-        """Returns the current particle positions"""
-
+        """Returns the current particle positions
+        SystemState.positions.shape = (num_particles, num_dimensions)"""
         return self.__positions
 
+    @property
     def neighbours(self):
         """Returns the current neighbours list"""
-
         return self.__neighbours
 
 
 class Electrostatic:
-
-    """
-    Represent the electrostatics information of the system
+    """Represent the electrostatics information of the system
 
     __forces:
     __potentials:
     __energies:
     """
-
     def __init__(self):
         # think
         pass
