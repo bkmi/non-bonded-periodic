d5b3bfc900d99eb7efc22a2f4d62ddf05c74e8f3
Ben <benmiller1315@gmail.com>
2017-12-25 15:25:35 +0100

    Updated variables and initilization data flow in the sysmodule

--- nbp/sysmodule.py
+++ nbp/sysmodule.py
@@ -1,8 +1,11 @@
+import numpy as np
+
+
 class System:
     """Wrapper for static SystemInfo and state dependent SystemState info."""
-    def __init__(self, info, states):
-        self.__systemInfo = info
-        self.__systemStates = states
+    def __init__(self, characteristic_length, sigma, particle_charges, positions):
+        self.__systemInfo = SystemInfo(characteristic_length, sigma, particle_charges)
+        self.__systemStates = [SystemState(positions)]
 
     def update_state(self, new_state):
         """Appends the new state to the systemStates list"""
@@ -24,26 +27,31 @@ class System:
 class SystemInfo:
     """This class represents all the static information of the system
 
-    box_dim: an array 1, 2 or 3 dimensional, each cell is a dimension of the box containing the system [W, L, H]
+    characteristic_length = L in the notes
     sigma: Constant related to lennard jones
     cutoff_radius: the radius chosen to do the cutoff
     epsilon0: physical constant
     particle_charges: Arranged like position: (row, columns) == (particle_num, charge_value)
-    L = constant used in calculation
-    V = constant used in calculation
     """
-    def __init__(self, box, sigma, particle_charges, L, V):
-        self.__box_dim = box
+    def __init__(self, characteristic_length, sigma, particle_charges):
         self.__sigma = sigma
         self.__cutoff_radius = sigma * 2.5  # sigma * 2.5 is a standard approximation
         self.__epsilon0 = 1
         self.__particle_charges = particle_charges
-        self.__L = L
-        self.__V = V
+        self.__char_length = characteristic_length
+
+    def char_length(self):
+        """Return the characteristic length aka L"""
+        return self.__char_length
 
     def box_dim(self):
-        """Gives the box dimensions"""
-        return self.__box_dim
+        """Gives the box dimensions.
+        box_dim: a list, 3 dimensional, each cell is a dimension of the box containing the system [W, L, H]"""
+        return [self.__char_length, self.__char_length, self.__char_length]
+
+    def volume(self):
+        """Returns the volume of the cell."""
+        return self.__char_length**3
 
     def cutoff(self):
         """Returns the value chosen for the cutoff radius"""
@@ -57,12 +65,6 @@ class SystemInfo:
 
     def particle_charges(self):
         return self.__particle_charges
-
-    def L(self):
-        return self.__L
-
-    def V(self):
-        return self.__V
 
 
 class SystemState:
