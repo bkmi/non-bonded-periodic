be9c5915e267e15027ef73f5bcac93838ec0ac60
Ben <benmiller1315@gmail.com>
2018-01-08 15:24:19 +0100

    minor changes to neighbours

--- nbp/neighbours.py
+++ nbp/neighbours.py
@@ -1,4 +1,3 @@
-from sysmodule import System
 import math
 
 
@@ -10,7 +9,7 @@ class Neighbours:
         :system: Instance of class System
         """
         self.System = system
-        self.__neighbour_list = self.__create_neighbours()
+        self.__create_neighbours()
 
     def __create_subcells(self):
         """
@@ -19,8 +18,8 @@ class Neighbours:
         """
         # define skin radius by using sigma.
         # The 3 is just an option that mostly works (?)
-        __skin_radius = self.System.info.sigma * 3
-        __box_length = self.System.state.char_length
+        __skin_radius = self.System.info().sigma() * 3
+        __box_length = self.System.info().char_length()
 
         self.__subcells_inrow = 1  # initialize
 
@@ -30,8 +29,6 @@ class Neighbours:
 
         self.__subcell_length = __box_length / self.__subcells_inrow
 
-        pass
-
     def __create_neighbours(self):
         """
         Creates the neighbours list by creating a head list which
@@ -40,23 +37,19 @@ class Neighbours:
         which belong to each box.
         :return: neighbour list
         """
-        particle_number = self.System.state.positions().shape[0]
+        particle_number = self.System.state().positions().shape[0]
         # create list (head) for starting index of subcell.
         # ^3 because we have 3 dimensions for 2 it would be ^2.
-        start_index = [0] * (self.__subcells_inrow ^ 3)
+        start_index = [0] * (self.__subcells_inrow ** 3)
         # create linked neighbour list
         self.__neighbour_list = []
         # get positions
         positions = self.System.state.positions()
 
-        for i in range(0, particle_number - 1):
-
+        for i in range(particle_number):
             subcell_id = self.__find_subcell(positions[i])
-
             self.__neighbour_list[i] = start_index[subcell_id]
             start_index[subcell_id] = i
-
-        return self.__neighbour_list
 
     def __find_subcell(self, position):
         """
@@ -66,25 +59,19 @@ class Neighbours:
         :return: subcell_id
         """
         subcell_id = [-1, -1, -1]
-        for axis in range(0, 2):
-            subcell_id[axis] = math.floor(position[axis]
-                                          / self.__subcell_length)
+        for axis in range(3):
+            subcell_id[axis] = math.floor(position[axis] / self.__subcell_length)
 
-        if (subcell_id[0] < 0 | subcell_id[1] < 0
-                | subcell_id[2] < 0):
-            print("Error: subcell_id value is negative!")
-            # throw exception?
+        if subcell_id[0] < 0 | subcell_id[1] < 0 | subcell_id[2] < 0:
+            raise ValueError('subcell_id value is negative')
         else:
             m = self.__subcells_inrow
-            subcell_id = subcell_id[0] \
-                + (subcell_id[1] - 1) * m \
-                + (subcell_id[2] - 1) * m ^ 2
+            subcell_id = subcell_id[0] + (subcell_id[1] - 1) * m + (subcell_id[2] - 1) * m ** 2
 
         return subcell_id
 
-    def update_neighbours(self):
+    def neighbours(self):
         """
-        :return:new neighbour list.
+        :return: retrieve neighbour list.
         """
-
         return self.__neighbour_list
