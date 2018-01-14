5c709bbc9523067550cb558e895bc612f1a86d61
SAlexey <shestakov.alexy@gmail.com>
2018-01-14 17:07:06 +0100

    vis tools, simulator

--- nbp/markov.py
+++ nbp/markov.py
@@ -1,8 +1,9 @@
 import numpy as np
-
+from scipy import stats
+from .sysmodule import SystemState
 
 class MCMC:
-    """An class which applies actor to a System instance and updates it to the next step in MCMC."""
+    """A class which applies actor to a System instance and updates it to the next step in MCMC."""
     def __init__(self, system, actor):
         self.__system = system
         self.__actor = actor
@@ -28,7 +29,7 @@ class Actor:
         simulate, accordingly to the phase is it in"""
         raise NotImplementedError('Actor is merely an interface, choose an implementation of Actor.')
 
-    def __check(self):
+    def __check(self, state, temperature):
         """Checks if the new proposed system is to be accepted"""
         raise NotImplementedError('Actor is merely an interface, choose an implementation of Actor.')
 
@@ -43,8 +44,8 @@ class Optimizer(Actor):
     def __propose(self):
         """Propose the next state"""
         last_state = self.__system.states[-1].positions
-        next_state = last_state + self.__system.electrostatics.forces + scipy.stats.multivariate_normal(
-            numpy.zeros(last_state.size), numpy.eye(last_state.shape[1])).rvs(1)
+        next_state = last_state + self.__system.electrostatics.forces + stats.multivariate_normal(
+            np.zeros(last_state.size), np.eye(last_state.shape[1])).rvs(1)
         return next_state
 
     def act(self, temperature):
@@ -62,15 +63,33 @@ class Optimizer(Actor):
 class Simulator(Actor):
     """The class that simulates."""
     def __init__(self, system, epsilon, sigma, n_steps=100, dt=0.001, length=1000):
-        super().__init__(system, epsilon, sigma, n_steps, dt, length)
+        super(Simulator, self).__init__(system, epsilon, sigma, n_steps, dt, length)
 
     def act(self, temperature):
         """Overriding of the function act of the Actor in order for it to simulate"""
-        pass
+        cov = 1     #TODO scale covariance
+        num_particles = len(self.system.state().positions())
+        indices_toMove = list(set(np.random.randint(num_particles, size=np.random.randint(1, num_particles))))
+        proposal_state = self.__metropolis(indices_toMove, cov)
+        if self.__check(proposal_state, temperature):
+            self.system.update_state(proposal_state)
+        else:
+            self.system.update_state(self.system.state())
 
-    def __check(self):
-        pass
+    def __check(self, state, temperature):
+        """Checks for the acceptance of a proposal state"""
+        beta = 1  # TODO: Calculate beta (with dim=1) used in Boltzmann Factor
+        energy_prev = self.system.states()[-1].energy()
+        energy_curr = state.energy()
+        p_acc = beta * (energy_curr-energy_prev)
+        if np.random.random() <= p_acc:
+            return True
+        else:
+            return False
 
-    def __metropolis(self):
+    def __metropolis(self, indices, cov):
         """Proposes the new states"""
-        pass
+        new_positions = np.copy(self.system.state().positions())
+        new_positions[indices] = np.array([stats.multivariate_normal(each, cov=cov).rvs().tolist() for each in new_positions[indices]])
+        proposal_state = SystemState(new_positions)
+        return proposal_state
