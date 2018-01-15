77dd435f3e8315c0e472fb30e61c05c6b49f29e6
Ben <benmiller1315@gmail.com>
2018-01-15 12:40:35 +0100

    updated test_neighbours from master

--- nbp/tests/test_neighbours.py
+++ nbp/tests/test_neighbours.py
@@ -1,36 +1,44 @@
 import nbp
-import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 
-def test_neighbours():
-    # random positions and charges (charges are not needed here)
-    positions = 4 * np.random.random_sample((10000, 3))
-    charges = np.random.random_sample((100, 1))
-    # random char length and sigma
-    system_info = nbp.SystemInfo(6, 0.2, charges)
+def test_neighbours(positions, particle_number, char_length, sigma, x):
+    charges = np.random.random_sample((particle_number, 1))
+    system_info = nbp.SystemInfo(char_length, sigma, charges)
     system_state = nbp.SystemState(positions)
     neighbours = nbp.Neighbours(system_info, system_state)
-    neighbours_list = neighbours.get_neighbours(positions[78])
+    neighbours_list = neighbours.get_neighbours(positions[x])
 
     return neighbours_list
 
 
-def show_frame(coordinates):
-    """a function to visualize the particles in 3d"""
+positions_set1 = 4 * np.random.random_sample((100, 3))
+nb_result = test_neighbours(positions_set1, len(positions_set1), 5, 0.5, 8)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(coordinates[:, 0],
-               coordinates[:, 1],
-               coordinates[:, 2], )
-    plt.show()
+# Call the named tuple and show the positions and the distance
+print("position result 1:", nb_result.nb_pos)
+print("distance result 1:",  nb_result.nb_dist)
+print("------------------End Test 1 ----------------------------- \n")
+
+k = 0
+if k is 0:
+    # Test if a real neighbour is seen as one.
+    positions_set2 = [[0, 0, 0], [1, 1.5, 1.2], [10, 10, 10]]
+    positions_set2 = np.asarray(positions_set2)
+    nb_result2 = test_neighbours(positions_set2, len(positions_set2), 10, 1, 0)
+
+    # Call the named tuple and show the positions and the distance
+    print("position result 2:", nb_result2.nb_pos)
+    print("distance result 2:",  nb_result2.nb_dist)
+    print("--------------End Test 2 ----------------------------- \n")
 
 
-nb_result = test_neighbours()
+## Test 2 if a real neighbour is seen as one.
+positions_set3 = [[5, 5, 5], [5, 5, 8]]
+positions_set3 = np.asarray(positions_set3)
+nb_result3 = test_neighbours(positions_set3, len(positions_set3), 10, 1, 0)
 
-print("position result:", nb_result.nb_pos)
-print("distance result:",  nb_result.nb_dist)
-
-#show_frame(coord)
+# Call the named tuple and show the positions and the distance
+print("position result 3:", nb_result3.nb_pos)
+print("distance result 3:",  nb_result3.nb_dist)
+print("---------------End Test 3 ----------------------------- \n")
