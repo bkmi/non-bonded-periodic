ec2a7c204ecc3c30276bcf22947a23544eca1ece
Ben <benmiller1315@gmail.com>
2018-01-23 14:26:20 +0100

    added neighbour flag, seperated LJ from ewald, added num_particles in system_info

--- example.py
+++ example.py
@@ -1,8 +1,10 @@
 import nbp
 import numpy as np
 
+# Example using LJ potential only1
+
 characteristic_length = 20
-sigma = 2
+sigma = 0
 particle_charges = np.asarray([1, 1, 1])
 positions = np.asarray([[1, 1, 1], [2,2,2], [4,5,10]])
 
