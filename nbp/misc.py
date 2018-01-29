192cf066b69a1608df5fc244eb6ddb7ec8308b3b
Alexey Shestakov <shestakov@jordan.imp.fu-berlin.de>
2018-01-29 14:01:26 +0100

    added energy plotter

--- nbp/misc.py
+++ nbp/misc.py
@@ -44,3 +44,49 @@ def play_frames(system, start=None, end=None, dt=None):
         plt.pause(pausetime)
     plt.close()
 
+class EnergyPlotter:
+    """A class for plotting energies"""
+    def __init__(self, system):
+        import matplotlib.pyplot as plt
+        self._system = system
+        self._states = self._system.states()
+        self._energies = None
+
+
+    def get_energies(self):
+        if self._energies == None:
+            self._energies = dict()
+            self._energies['total'] = list(map(lambda x: x.energy(), self._states))
+            self._energies['lj'] = list(map(lambda x: x.energy_lj(), self._states))
+            self._energies['coulomb'] = list(map(lambda x: x.energy_ewald(), self._states))
+
+    def _plot_e_clmb(self, canvas):
+        canvas.plot(self._energies['coulomb'])
+        plt.show()
+
+    def _plot_e_lj(self, canvas):
+        canvas.plot(self._energies['lj'])
+        plt.show()
+
+    def _plot_e_tot(self, canvas):
+        canvas.plot(self._energies['total'])
+        plt.show()
+
+    def plot_energies(self, typ='total', start=0, end=None):
+        fig = plt.figure(1)
+        ax = fig.add_subplot(111)
+        if end == None:
+            end = len(self._states)
+        self.get_energies()
+        if typ == 'total':
+            ax.label = 'Total Energy'
+            self._plot_e_tot(ax)
+        elif typ == 'lj':
+            ax.label = 'Energy LJ'
+            self._plot_e_lj(ax)
+        elif typ == 'coulomb':
+            ax.label = 'Energy Coulomb'
+            self._plot_e_clmb(ax)
+
+
+
