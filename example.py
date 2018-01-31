f8f405c7661118bec6a3ebc09632f0e923e4b04b
Ben <benmiller1315@gmail.com>
2018-01-31 22:18:18 +0100

    added multi level optimization to example

--- example.py
+++ example.py
@@ -16,17 +16,22 @@ from nbp.tests.tools import make_system
 # blah
 
 # np.random.RandomState(6)
-characteristic_length = 10
+characteristic_length = 1000
 positions = (np.asarray([[1, 0, -2**(-1/2)],
                         [-1, 0, -2**(-1/2)],
                         [0, 1, 2**(-1/2)],
-                        [0, -1, 2**(-1/2)]]) + 5) * 1.5
+                        [0, -1, 2**(-1/2)]]) + characteristic_length/2) * 3
 system = make_system(characteristic_length=characteristic_length,
                      positions=positions,
                      lj=True,
                      ewald=False,
                      use_neighbours=False)
-system.optimize(num_particles=4)
+system.optimize(cov=system.info().cutoff()/24,
+                num_particles=4)
+print(len(system.states())
+print(system.state().distances_unwrapped())
+system.optimize(cov=system.info().cutoff()/2**8,
+                num_particles=4)
 print(len(system.states()))
 print(system.state().distances_unwrapped())
 print(system.state().distances_wrapped())
