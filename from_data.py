d2988bc9c5f2a1c91f91d115e450d53ec8e5874f
Ben <benmiller1315@gmail.com>
2018-02-22 20:42:58 +0100

    basic units

--- from_data.py
+++ from_data.py
@@ -7,12 +7,19 @@ for key, val in data.items():
     if isinstance(val, np.ndarray):
         print(key, val.shape)
         print('flipped ', val[:,None].shape)
+    elif isinstance(val, dict):
+        for sk, sv in val.items():
+            print(sk, sv)
+
+# units epsilon0 = 55.3e-4 eV/A
+# 1 kJ/mol = 1.0364x10-2 eV
 
 system = nbp.System(data['ch_length'],
                     data['sigma'][:,None],
-                    data['epsilon'][:,None],
+                    data['epsilon'][:,None] * 1.0364e-2,
                     data['charge'][:,None],
                     data['pos'],
-                    reci_cutoff=10, lj=True, ewald=True, use_neighbours=False)
+                    reci_cutoff=10, lj=True, ewald=True, use_neighbours=False,
+                    epsilon0=55.3e-4)
 op_sys = system.optimize()
 simu_sys = op_sys.simulate(1000, 200)
